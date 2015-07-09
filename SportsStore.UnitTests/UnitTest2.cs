a50256a9cbc49daca7f0081ca3b4ff117c984fb6
alweky <alweky@yahoo.ca>
2015-07-09 17:15:54 -0400

    Finished Chapter 7
    Still a small bug with number of pages in the bottom, will try to fix
    and look for one

---
+++ SportsStore.UnitTests/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
